10d8e2b0bf2c7a71336ba27c1cd414518a77adf9
javierrollan <jrollanb@gmail.com>
2021-09-24 15:14:47 +0200

    README.md updated, Ex05 completed

---
+++ Exercise05/README.md
@@ -0,0 +1,1 @@
+# Exercise 05: More Variables and Printing
---
+++ Exercise05/ex05.py
@@ -0,0 +1,14 @@
+name = 'Peter'
+age = 30
+height = 178
+weight = 72
+eyes = 'green'
+hair = 'Brown'
+
+print(f"Lets talk about {name}")
+print(f"He is {height} cm tall")
+print(f"He is {weight} kg")
+print(f"He's got {eyes} eyes and {hair} hair.")
+
+total = age + height + weight
+print(f"If We add {age}, {height} and {weight} the total is {total}")
